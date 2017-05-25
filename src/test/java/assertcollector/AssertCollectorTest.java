b65df40703d30d09462e382f6b2133eaf9c638e5
gkushang <gkushang@gmail.com>
2017-05-25 18:00:56 -0400

    add tests

--- src/test/java/assertcollector/AssertCollectorTest.java
+++ src/test/java/assertcollector/AssertCollectorTest.java
@@ -2,30 +2,83 @@ package assertcollector;
 
 import static assertcollector.AssertCollector.throwAnyFailures;
 import static assertcollector.AssertCollector.verifyThat;
+import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
+import java.io.IOException;
+
+import org.apache.commons.lang.StringUtils;
 import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import utils.Account;
 
 public class AssertCollectorTest
 {
+    private Account account;
+
+    @Before
+    public void setup()
+                    throws IOException
+    {
+        ObjectMapper mapper = new ObjectMapper();
+        account = mapper.readValue( getClass().getClassLoader().getResource( "account.json" ), Account.class );
+    }
+
     @Test
-    public void verifyThatWithMessageTest()
+    public void regressTheAccount_jsonObject()
+    {
+        // BUG with firstName. AssertCollector should capture the bug and should continue to capture more bugs
+        // Traditional Assert will fail at this point and will not continue to capture more bugs, but we have more bugs in the JSON object
+        verifyThat( "First name should be valid", account.firstName, is( "valid" ) );
+
+        verifyThat( "Last name should be valid", account.lastName, is( "valid" ) );
+
+        // BUG with username, and should be captured by AssertCollector
+        verifyThat( "Username should be valid", account.username, is( "valid" ) );
+
+        verifyThat( "Email should be provided", account.email, is( not( StringUtils.EMPTY ) ) );
+
+        // BUG with mustBeTrue, and should be captured by AssertCollector
+        verifyThat( "MustBeTrue should be what it is", account.mustBeTrue, is( true ) );
+
+        // Total bugs captured by AssertCollector is 3 from Single Execution. Traditional assertions, e.g. Assert, would have only captured & reported bug with firstName
+        assertThat( "AssertCollector should capture 3 bugs", AssertCollector.getNumberOfErrors(), is( 3 ) );
+
+        //throwAnyFailures should report the detailed error messages with all 3 bugs
+        try
+        {
+            throwAnyFailures();
+        }
+        catch ( Throwable e )
+        {
+            assertThat( e.getMessage().trim(),
+                            is( "Number of failure(s) : 3\n\nFirst name should be valid\nExpected: is \"valid\""
+                                            + "\n     but: was \"invalid\"\n\nUsername should be valid\nExpected: is \"valid\""
+                                            + "\n     but: was \"invalid\"\n\nMustBeTrue should be what it is\nExpected: is <true>"
+                                            + "\n     but: was <false>" ) );
+        }
+    }
+
+    @Test
+    public void verifyThatWithMessage_test()
     {
         verifyThat( "error message", "Hello", is( "Hello" ) );
         throwAnyFailures();
     }
 
     @Test( expected = AssertionError.class )
-    public void throwAnyFailuresTest()
+    public void throwAnyFailures_test()
     {
         verifyThat( "error message", 3, is( 2 ) );
         throwAnyFailures();
     }
 
     @Test
-    public void throwAnyFailuresMessageTest()
+    public void throwAnyFailuresMessage_test()
     {
         verifyThat( "Greeting is incorrect", "Hi, there", is( "Hello, there" ) );
         try
@@ -40,7 +93,7 @@ public class AssertCollectorTest
     }
 
     @Test
-    public void verifyThatWithoutMessageTest()
+    public void verifyThatWithoutMessage_test()
     {
         verifyThat( "Hello", is( "Hello" ) );
         throwAnyFailures();
---
+++ src/test/java/utils/Account.java
@@ -0,0 +1,15 @@
+package utils;
+
+public class Account
+{
+    public String firstName;
+
+    public String lastName;
+
+    public String email;
+
+    public String username;
+
+    public boolean mustBeTrue;
+
+}
